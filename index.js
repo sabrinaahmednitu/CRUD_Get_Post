faa939995181f4b633acfae81eae9961db0c9de8
sabrina <sabrinaahmednitu@gmail.com>
2023-07-29 16:05:35 +0600

    delete one done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -44,8 +45,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query={_id: mongoObjectId(id)}
+      const result = await crudCollection.deleteOne(query);
+      res.send(result);
+    })
+ 
 
-    
   } finally {
     
   }
